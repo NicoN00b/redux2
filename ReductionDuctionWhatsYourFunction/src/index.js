a8163ce80b6c19ae66578a31fd945fc4e29a170f
nr <nicholas.raethke@gmail.com>
2018-04-24 23:36:04 -0700

    use subscribe to persist data using local storage as initial state

--- ReductionDuctionWhatsYourFunction/src/index.js
+++ ReductionDuctionWhatsYourFunction/src/index.js
@@ -3,7 +3,7 @@ import appReducer from './store/reducers'
 // import initialState from './initialState.json'
 import { createStore } from 'redux'
 
-const initialState = JSON.parse(localStorage['redux-store']) ?
+const initialState = (localStorage['redux-store']) ?
     JSON.parse(localStorage['redux-store']) :
     {}
 
@@ -16,19 +16,4 @@ store.subscribe(() => {
 
   const state = JSON.stringify(store.getState())
   localStorage['redux-store'] = state
-})
-
-store.dispatch({
-  type: C.ADD_DAY,
-  payload: {
-    "resort": "Mt Shasta",
-    "date": "2016-10-28",
-    'powder': false,
-    "backcountry": true
-  }
-})
-
-store.dispatch({
-  type: C.SET_GOAL,
-  payload: 2
 })
