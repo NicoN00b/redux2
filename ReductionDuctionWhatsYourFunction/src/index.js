c58c687070190a9883ea715033f32633182c9833
nr <nicholas.raethke@gmail.com>
2018-04-25 12:23:07 -0700

    create middleware

--- ReductionDuctionWhatsYourFunction/src/index.js
+++ ReductionDuctionWhatsYourFunction/src/index.js
@@ -1,19 +1,45 @@
 import C from './constants'
-import appReducer from './store/reducers'
-// import initialState from './initialState.json'
-import { createStore } from 'redux'
+import storeFactory from './store'
 
 const initialState = (localStorage['redux-store']) ?
     JSON.parse(localStorage['redux-store']) :
     {}
 
-const store = createStore(appReducer, initialState)
-
-window.store = store
-
-
-store.subscribe(() => {
-
+const saveState = () => {
   const state = JSON.stringify(store.getState())
   localStorage['redux-store'] = state
+}
+
+const store = storeFactory(initialState)
+
+store.subscribe(saveState)
+
+store.dispatch({
+  type: C.ADD_DAY,
+  payload: {
+    "resort": "Mt Shasta",
+    "date": "2016-10-28",
+    "powder": true,
+    "backcountry": false
+  }
+})
+
+store.dispatch({
+  type: C.ADD_DAY,
+  payload: {
+    "resort": "Squaw Valley",
+    "date": "2016-1-28",
+    "powder": true,
+    "backcountry": false
+  }
+})
+
+store.dispatch({
+  type: C.ADD_DAY,
+  payload: {
+    "resort": "The Canyons",
+    "date": "2016-11-28",
+    "powder": false,
+    "backcountry": true
+  }
 })
